ee129b76b5bfbfa045494fb28cedaa97bafbb677
gokulrameshd <gorad364@gmail.com>
2020-11-21 10:00:35 +0530

    dir

--- test_session11.py
+++ test_session11.py
@@ -42,8 +42,8 @@ def test_p2j_1():
 
 def test_crop_px():
     crop(scr_folder = "images",dest_folder = "cpx",px =50,per = None)
-    a_images = glob.glob("images/*.*")
-    o_images = glob.glob("cpx/*.*")
+    a_images = glob.glob("./images/*.*")
+    o_images = glob.glob("./cpx/*.*")
     im1 = Image.open(o_images[0]) 
     im2 = Image.open(os.path.join("images",o_images[0].split("\\")[-1]) )
     assert im1.size[0] < im2.size[0]
@@ -51,32 +51,34 @@ def test_crop_px():
 
 def test_crop_per():
     crop(scr_folder = "images",dest_folder = "cper",px =None,per = 50)
-    a_images = glob.glob("images/*.*")
-    o_images = glob.glob("cper/*.*")
+    a_images = glob.glob("./images/*.*")
+    o_images = glob.glob("./cper/*.*")
     im1 = Image.open(o_images[0]) 
     im2 = Image.open(os.path.join("images",o_images[0].split("\\")[-1]) )
     assert im1.size[0] < im2.size[0]
 
 def test_res_p():
     propation_resize_dir(scr_folder = "images" ,dest_folder = 'res_p' , percentage = 20 ,w_per = 0,h_per = 0)
-    a_images = glob.glob("images/*.*")
-    o_images = glob.glob("res_p/*.*")
+    a_images = glob.glob("./images/*.*")
+    o_images = glob.glob("./res_p/*.*")
     im1 = Image.open(o_images[0]) 
     im2 = Image.open(os.path.join("images",o_images[0].split("\\")[-1]) )
     assert im1.size[0] < im2.size[0]
 
 def test_res_w_per():
     propation_resize_dir(scr_folder = "images" ,dest_folder = 'w_per' , percentage = 0,w_per = 10,h_per = 0)
-    a_images = glob.glob("images/*.*")
-    o_images = glob.glob("w_per/*.*")
+    a_images = glob.glob("./images/*.*")
+    o_images = glob.glob("./w_per/*.*")
     im1 = Image.open(o_images[0]) 
     im2 = Image.open(os.path.join("images",o_images[0].split("\\")[-1]) )
     assert im1.size[1] < im2.size[1]
 
 def test_res_h_per():
     propation_resize_dir(scr_folder = "images" ,dest_folder = 'h_per' , percentage = 0,w_per = 0,h_per = 30)
-    a_images = glob.glob("images/*.*")
-    o_images = glob.glob("h_per/*.*")
+    a_images = glob.glob("./images/*.*")
+    o_images = glob.glob("./h_per/*.*")
     im1 = Image.open(o_images[0]) 
-    im2 = Image.open(os.path.join("images",o_images[0].split("\\")[-1]) )
+    im2 = Image.open(os.path.join("./images",o_images[0].split("\\")[-1]) )
     assert im1.size[0] < im2.size[0]
+
+# def test_error():
